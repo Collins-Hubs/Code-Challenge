a48f657b547ffa1fb434868d90c78f807e1aef3f
Collins <collinskibet7545@gmail.com>
2024-08-04 23:40:27 +0300

    First commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,16 +1,16 @@
 import React from "react";
-import TransactionsList from "./TransactionsList";
+//import TransactionsList from "./TransactionsList";
 import Search from "./Search";
-import AddTransactionForm from "./AddTransactionForm";
+//import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-  return (
-    <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
-    </div>
-  );
+    return (
+        <div>
+            <Search />
+            {/**<AddTransactionForm />
+            <TransactionsList />**/}
+        </div>
+    );
 }
 
 export default AccountContainer;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState, useDeferredValue } from "react";
+import TransactionsList from "./TransactionsList";
+import AddTransactionForm from "./AddTransactionForm";
 
 function Search() {
-  return (
-    <div className="ui large fluid icon input">
-      <input
-        type="text"
-        placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
-      />
-      <i className="circular search link icon"></i>
-    </div>
-  );
+    const [searchValue, setSearchValue] = useState("")
+    const deferredSearchValue = useDeferredValue(searchValue);
+
+    return (
+        <div>
+            <div className="ui large fluid icon input">
+                <input
+                    type="text"
+                    placeholder="Search your Recent Transactions"
+                    onChange={(e) => { setSearchValue(e.target.value) }}
+
+                />
+                <i className="circular search link icon"></i>
+            </div>
+            <AddTransactionForm />
+            <TransactionsList searchValue={deferredSearchValue} />
+        </div>
+    );
 }
 
 export default Search;
